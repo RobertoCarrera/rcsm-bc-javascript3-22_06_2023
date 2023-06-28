71cbfc1eecb7039dd54193a832e2aa945228c31f
RobertoCarrera <robertocarrera93@gmail.com>
2023-06-29 00:15:18 +0200

    Acabado

--- js/Ejercicio12.js
+++ js/Ejercicio12.js
@@ -5,3 +5,46 @@
 // guardarse en un nuevo array. Por ejemplo, en un array de 10 posiciones le 
 // indicamos mostrar los números acabados en 5, podría salir 155, 25, etc.
 
+let length = prompt("Dame el tamaño del array: ");
+let array = [];
+let arrayDigit = [];
+
+const fillArray = (newLength) => {
+
+    let array = [];
+
+    for (let i = 0; i < newLength; i++) {
+        
+        array[i] = Math.floor(Math.random()*300)+1;
+    }
+
+    return array;
+}
+
+const searchDigit = (newArray) => {
+
+    let digit = 0;
+    let arrayDigits = [];
+    let digitsCounter = 0;
+
+    do {
+
+        digit = prompt("Dime el dígito final que quieres que busque (0-9):");
+        
+    } while ((digit<0)||(digit>9));
+
+    for (let i = 0; i < newArray.length; i++) {
+        
+        if(newArray[i]%10 == digit){
+
+            arrayDigits[digitsCounter] = newArray[i];
+        }
+    }
+    
+    return arrayDigits;
+}
+
+array = fillArray(length);
+console.log(array);
+arrayDigit = searchDigit(array);
+console.log(arrayDigit);
